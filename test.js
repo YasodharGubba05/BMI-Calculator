71c59e6d69ee05bb519c77775b9a60fde2fc99c1
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2025-01-09 14:59:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -13,6 +13,22 @@ form.addEventListener("submit",function(e){
       else if (isNaN(weight) || weight <= 0) {
         results.innerHTML = "<p>Please enter a valid weight</p>";
       }
-   
+    
+    else{
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        let message = `<span>Your BMI is: ${bmi}</span><br/>`; 
+    
+        
+        if (bmi < 18.5) {
+          message += "<p>You are underweight</p>";
+        } else if (bmi > 24.9) {
+          message += "<p>You are overweight</p>";
+        } else {
+          message += "<p>Your weight is in the normal range</p>";
+        }
+    
+       
+        results.innerHTML = message;
+    }
     
 })
