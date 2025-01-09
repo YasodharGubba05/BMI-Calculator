68d650f9bdc41ca70939837556242a71a81e3bff
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2025-01-09 14:58:16 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,17 @@ const form=document.querySelector("form")
 
 form.addEventListener("submit",function(e){
     e.preventDefault()
-    const height=document.querySelector("#height")
-    const weight=document.querySelector("#weight")
+    const height=parseInt(document.querySelector("#height").value);
+    const weight=parseInt(document.querySelector("#weight").value)
+    const results=document.querySelector("#results")
+   
+    if (isNaN(height) || height <= 0) {
+        results.innerHTML = "<p>Please enter a valid height</p>";
+      }
+      
+      else if (isNaN(weight) || weight <= 0) {
+        results.innerHTML = "<p>Please enter a valid weight</p>";
+      }
+   
     
-
 })
