7217bf7651ae8c2daeb7d810b0c224b4253ba521
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2025-01-09 14:51:04 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
+const form=document.querySelector("form")
 
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+    const height=document.querySelector("#height")
+    const weight=document.querySelector("#weight")
+    
+
+})
